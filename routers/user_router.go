2fa6a23c4a085ab0e9920f5848bbf2c3edeba4f0
StephenZzz <stephenzhao94@163.com>
2023-06-12 17:29:36 +0800

    add add login status check

--- routers/user_router.go
+++ routers/user_router.go
@@ -13,7 +13,7 @@ func SetUpUserRouters(r *gin.Engine) {
 	r.GET("/hello", user.Hello)
 
 	r.POST("/users", user.CreateUser)
-	r.GET("/users", user.GetAllUser)
+	r.GET("/users", middlewares.CheckLogin(), user.GetAllUser)
 	r.POST("/users/login", user.UserLogin)
 	r.DELETE(("/users/:id"), middlewares.CheckLogin(), user.DeletedUser)
 }
